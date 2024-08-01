a879ac6a67c30372639da81a16f08ac3e1bfcde2
morning120429 <forever120429@gmail.com>
2024-08-01 06:05:50 -0400

    update for multi file

--- app.py
+++ app.py
@@ -1,44 +1,49 @@
 import sys
 from PyQt6 import uic
-from PyQt6.QtCore import QThread, pyqtSignal
+from PyQt6.QtCore import QThread, pyqtSignal, Qt
 from PyQt6.QtWidgets import QMainWindow, QApplication, QFileDialog, QMessageBox
 from detect import *
 from time import sleep
 
-Ui_MainWindow, QtBaseClass = uic.loadUiType('converter.ui')
+Ui_MainWindow, QtBaseClass = uic.loadUiType('multi-converter.ui')
 
 class Worker(QThread):
     finished = pyqtSignal()
     alert_signal = pyqtSignal(str)
 
-    def __init__(self, file_path, output_file_name, parent=None):
+    def __init__(self, files_path, output_file_name, parent=None):
         super().__init__(parent)
-        self.file_path = file_path
+        self.files_path = files_path
         self.output_file_name = output_file_name
 
     def run(self):
-        try:
-            table_df = extract_table_from_excel(self.file_path)
-            print(table_df)
+        file_number = 1
+        for file_path in self.files_path:
+            try:
+                table_df = extract_table_from_excel(file_path)
+                print(table_df)
+                
+                check_date_list = ['trade_date', 'TransactionDate', 'Date', 'TD']
+                check_time_list = ['time', 'Activity Time']
+                check_symbol_list = ['instrument.cns_equity_master.symbol', 'Symbol']
+                check_quantity_list = ['quantity', 'Quantity']
+                check_price_list = ['price', 'Price']
+                check_commission_list = ['fee_commission', 'Commission', 'Fees & Comm']
+                check_action_list = ['side_direction', 'TransactionType', 'Action', 'Transaction']
+                complete_column(table_df, check_date_list, clean_date_format, 1)
+                complete_column(table_df, check_time_list, clean_time_format, 2)
+                complete_column(table_df, check_symbol_list, clean_symbol_format, 3)
+                complete_column(table_df, check_quantity_list, clean_quantity_format, 4)
+                complete_column(table_df, check_price_list, clean_price_format, 5)
+                complete_column(table_df, check_commission_list, clean_commission_format, 6)
+                complete_column(table_df, check_action_list, clean_action_format, 7)
+
+                wb.save(f"{self.output_file_name}-{file_number}.xlsx")
+                file_number += 1
+                sleep(1)
+            except Exception as e:
+                self.alert_signal.emit(str(e))
             
-            check_date_list = ['trade_date', 'TransactionDate', 'Date', 'TD']
-            check_time_list = ['time', 'Activity Time']
-            check_symbol_list = ['instrument.cns_equity_master.symbol', 'Symbol']
-            check_quantity_list = ['quantity', 'Quantity']
-            check_price_list = ['price', 'Price']
-            check_commission_list = ['fee_commission', 'Commission', 'Fees & Comm']
-            check_action_list = ['side_direction', 'TransactionType', 'Action', 'Transaction']
-            complete_column(table_df, check_date_list, clean_date_format, 1)
-            complete_column(table_df, check_time_list, clean_time_format, 2)
-            complete_column(table_df, check_symbol_list, clean_symbol_format, 3)
-            complete_column(table_df, check_quantity_list, clean_quantity_format, 4)
-            complete_column(table_df, check_price_list, clean_price_format, 5)
-            complete_column(table_df, check_commission_list, clean_commission_format, 6)
-            complete_column(table_df, check_action_list, clean_action_format, 7)
-            wb.save(f"{self.output_file_name}.xlsx")
-        except Exception as e:
-            self.alert_signal.emit(str(e))
-        
         self.finished.emit()
 
 
@@ -57,11 +62,7 @@ class MainWindow(QMainWindow):
 
     def upload_files(self):
         file_names, _ = QFileDialog.getOpenFileNames(self, "Select Files", "", "All Files (*)")
-        if file_names:
-            # Clear the QListWidget before adding new items
-            self.ui.files.clear()
-            # Add only the first selected file to the QListWidget
-            self.ui.files.addItem(file_names[0])
+        self.ui.files.addItems(file_names)
 
     def run_script_in_thread(self):
         # Check if QListWidget is empty
@@ -69,24 +70,40 @@ class MainWindow(QMainWindow):
             QMessageBox.warning(self, "Warning", "Please import an Excel or CSV file.")
             return
         
+        self.files_path = []
+        for i in range(self.ui.files.count()):
+            item = self.ui.files.item(i)
+            self.files_path.append(item.text())
+
+        # Check if the file is an Excel or CSV file
+        invalid_file_list = []
+        for item in self.files_path:
+            if not (item.endswith('.xlsx') or item.endswith('.xls') or item.endswith('.csv')):
+                invalid_file_list.append(item)
+
+        if len(invalid_file_list) != 0:
+            # Remove invalid files from QListWidget
+            for item in invalid_file_list:
+                items_to_remove = self.ui.files.findItems(item, Qt.MatchFlag.MatchExactly)
+                for remove_item in items_to_remove:
+                    row = self.ui.files.row(remove_item)
+                    self.ui.files.takeItem(row)
+
+            QMessageBox.warning(self, "Warning", "Unsupported file detected !  Removed automatically !")
+            return
+        
         if self.ui.output_name.text() == "":
             QMessageBox.warning(self, "Warning", "Please enter the output file name !")
             return
 
-        self.file_path = self.ui.files.item(0).text()
         self.output_file_name = self.ui.output_name.text()
-        
-        # Check if the file is an Excel or CSV file
-        if not (self.file_path.endswith('.xlsx') or self.file_path.endswith('.xls') or self.file_path.endswith('.csv')):
-            QMessageBox.warning(self, "Warning", "Unsupported file type! Please import an Excel or CSV file.")
-            return
         
         self.ui.convert_btn.setEnabled(False)
         self.ui.import_btn.setEnabled(False)
 
         sleep(3)
 
-        self.worker = Worker(self.file_path, self.output_file_name)
+        self.worker = Worker(self.files_path, self.output_file_name)
         self.worker.finished.connect(self.on_finished)
         self.worker.alert_signal.connect(self.show_alert)
         self.worker.start()
